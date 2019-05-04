3f011c5e9ea1746e429b230936fd9e3491810cb5
unknown <rmtestard@gmail.com>
2019-05-04 13:00:00 +0200

    action and reducer for GET_USERS

---
+++ server/client/src/actions/userActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+import { GET_USERS, USER_LOADING } from './types';
+
+// Get all Profiles
+export const getUsers = () => dispatch => {
+  dispatch(setUserLoading());
+  axios
+    .get(`/api/users`)
+    .then(res =>
+      dispatch({
+        type: GET_USERS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_USERS,
+        payload: null
+      })
+    );
+};
+
+// Profile loading
+export const setUserLoading = () => {
+  return {
+    type: USER_LOADING
+  };
+};

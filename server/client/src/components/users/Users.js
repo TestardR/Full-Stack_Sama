4f072f018d844502e3a6953c3279ae0265eee6a4
unknown <rmtestard@gmail.com>
2019-05-04 12:49:38 +0200

    adding Spinner while loading

---
+++ server/client/src/components/users/Users.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../common/Spinner';
+import { getProfiles } from '../../actions/profile';
+
+const UserItem = props => {
+  return <div />;
+};
+
+UserItem.propTypes = {};
+
+export default UserItem;
